7bc0e455a54ed849043d91b54046605d91a66a28
reviq <nway@o2.pl>
2022-04-11 01:33:35 +0200

    changing types in gymuser model from date to string

--- src/main/java/pl/nwaczykgym/nwaczykgym/model/GymUser.java
+++ src/main/java/pl/nwaczykgym/nwaczykgym/model/GymUser.java
@@ -3,27 +3,32 @@ package pl.nwaczykgym.nwaczykgym.model;
 import pl.nwaczykgym.nwaczykgym.Gender;
 import pl.nwaczykgym.nwaczykgym.TypeOfUser;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.util.Date;
 
 @Entity
 public class GymUser {
 
     @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
     private Long id;
     private String name;
     private String surname;
+
+    @Enumerated(EnumType.STRING)
     private Gender gender;
+
+    @Enumerated(EnumType.STRING)
     private TypeOfUser userType;
+    private Long pesel;
     private String address;
     private String city;
     private String postCode;
     private String contactNumber;
-    private Date dateOfBirth;
-    private Date registrationDate;
-    private Date gymAccessFrom;
-    private Date gymAccessTo;
+    private String dateOfBirth;
+    private String registrationDate;
+    private String gymAccessFrom;
+    private String gymAccessTo;
 
 
     public GymUser(Long id,
@@ -31,20 +36,22 @@ public class GymUser {
                    String surname,
                    Gender gender,
                    TypeOfUser userType,
+                   Long pesel,
                    String address,
                    String city,
                    String postCode,
                    String contactNumber,
-                   Date dateOfBirth,
-                   Date registrationDate,
-                   Date gymAccessFrom,
-                   Date gymAccessTo)
+                   String dateOfBirth,
+                   String registrationDate,
+                   String gymAccessFrom,
+                   String gymAccessTo)
     {
         this.id = id;
         this.name = name;
         this.surname = surname;
         this.gender = gender;
         this.userType = userType;
+        this.pesel = pesel;
         this.address = address;
         this.city = city;
         this.postCode = postCode;
@@ -53,6 +60,10 @@ public class GymUser {
         this.registrationDate = registrationDate;
         this.gymAccessFrom = gymAccessFrom;
         this.gymAccessTo = gymAccessTo;
+    }
+
+    public GymUser(){
+
     }
 
     public String getName() {
@@ -79,15 +90,15 @@ public class GymUser {
         return contactNumber;
     }
 
-    public Date getDateOfBirth() {
+    public String getDateOfBirth() {
         return dateOfBirth;
     }
 
-    public Date getGymAccessFrom() {
+    public String getGymAccessFrom() {
         return gymAccessFrom;
     }
 
-    public Date getGymAccessTo() {
+    public String getGymAccessTo() {
         return gymAccessTo;
     }
 
@@ -103,7 +114,11 @@ public class GymUser {
         return gender;
     }
 
-    public Date getRegistrationDate() {
+    public String getRegistrationDate() {
         return registrationDate;
+    }
+
+    public Long getPesel() {
+        return pesel;
     }
 }
